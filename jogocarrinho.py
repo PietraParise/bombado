fd43b6bc84f4f5ff7fe93b871497a0176d08402b
PietraParise <134699480+PietraParise@users.noreply.github.com>
2024-04-05 09:30:14 -0300

    Add files via upload

--- jogocarrinho.py
+++ jogocarrinho.py
@@ -47,3 +47,34 @@ if teclas[pygame.K_RIGHT] and personagem_posicao_x< largura_tela -personagem_ima
 obstaculo_posicao_y += obstaculo_velocidade
 
 personagem_rect = pygame.Rect(personagem_posicao_x, personagem_posicao_y, personagem_imagem.get_width(), personagem_imagem.get_height())
+obstaculo_rect = pygame.Rect(obstaculo_posicao_x, obstaculo_posicao_y, obstaculo_imagem.get_width(), obstaculo_imagem.get_height())
+if personagem_rect.colliderect(obstaculo_rect):
+    jogo_ativo= False
+
+if obstaculo_posicao_y > altura_tela:
+    obstaculo_posicao_x = random.randint(0, largura_tela - obstaculo_imagem.get_width())
+    obstaculo_posicao_y= -obstaculo_imagem.get_height()
+
+tela.fill(BRANCO)
+tela.blit(personagem_imagem, (personagem_posicao_x, personagem_posicao_y))
+tela.blit(obstaculo_imagem, (obstaculo_posicao_x, obstaculo_posicao_y))
+pygame.display.flip()
+
+if not jogo_ativo:
+    tela.fill(BRANCO)
+    fonte = pygame.font.Font(None, 36)
+    mensagem = fonte.render("Game Over! Pressione R para jogar novamente ou Q para sair.", True, PRETO)
+    tela.blit(mensagem, (largura_tela // 2 - mensagem.get_width() // 2, altura_tela // 2 - mensagem.get_height() // 2))
+    pygame.display.flip()
+
+    for evento in pygame.event.get():
+            if evento.type == pygame.KEYDOWN:
+                if evento.key == pygame.K_r:
+                    jogo_ativo = True
+                    personagem_posicao_x = largura_tela // 2 - personagem_imagem.get_width() // 2
+                    personagem_posicao_y = altura_tela - personagem_imagem.get_height()
+                    obstaculo_posicao_x = random.randint(0, largura_tela - obstaculo_imagem.get_width())
+                    obstaculo_posicao_y = -obstaculo_imagem.get_height()
+                elif evento.key == pygame.K_q:
+                    pygame.quit()
+                    sys.exit()
